06dcb46ade9d86ead71537117c134e5f494b03a2
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-01-11 10:44:52 -0800

    Initial commit
    Co-authored-by: Nataliya Zhuravleva<natalindria@gmail.com>

---
+++ Library/Models/LibraryContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Models
+{
+  public class LibraryContext : IdentityDbContext<ApplicationUser>
+  {
+    public DbSet<Librarian> Librarians { get; set; }
+    public DbSet<Book> Books { get; set; }
+    public DbSet<Author> Authors { get; set; }
+    public DbSet<BookAuthor> BookAuthor { get; set; }
+    public DbSet<Copy> Copies { get; set; }
+
+    public LibraryContext(DbContextOptions options) : base(options) { }
+  }
+}
